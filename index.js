03947871c1bd5463c91592022bdd22e030e682cd
Pixy <111642712+pixytje@users.noreply.github.com>
2024-09-24 15:15:28 +0200

    Better version.

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-fetch("http://185.228.82.174:30120/info.json")
+fetch("https://servers-frontend.fivem.net/api/servers/single/qg6a49")
     .then(response => response.json())
     .then(serverInfo => {
-        const serverName = serverInfo.vars.sv_projectName;
+        const serverName = serverInfo.Data.vars.sv_projectName;
 
-        fetch("http://185.228.82.174:30120/players.json")
-            .then(response => response.json())
-            .then(players => {
-                let bots = 0;
+        const players = serverInfo.Data.players;
+        let bots = 0;
 
-                players.forEach(player => {
-                    if (player.identifiers && player.identifiers.length > 0 && !player.identifiers[0].startsWith("license:")) {
-                        bots++;
-                    }
-                });
+        players.forEach(player => {
+            if (player.identifiers && player.identifiers.length > 0 && !player.identifiers[0].startsWith("license:")) {
+                bots++;
+            }
+        });
 
-                console.log(`${serverName} heeft ${bots} bots`);
-            })
-            .catch(error => {
-                console.error("Er is een fout opgetreden bij het ophalen van de spelersinformatie:", error);
-            });
+        console.log(`${serverName} heeft ${bots} bots`);
     })
     .catch(error => {
-        console.error("Er is een fout opgetreden bij het ophalen van de serverinfo:", error);
+        console.error("Error", error);
     });
